8ab3a24d2c028604a2ab741b070a8e65b09033f5
zhangshuxiao <zhangshuxiao@xiaomi.com>
2016-01-18 10:54:28 +0800

    When handling voice key, it may cause deadlock.
    handleVoiceKeyEventLocked:
        dispatchMediaKeyEventLocked(downEvent, needWakeLock, session);
        In this place might be scheduled,
        dispatchMediaKeyEventLocked(keyEvent, needWakeLock, session);
    dispatchMediaKeyEventLocked:
        ActivityManager.getCurrentUser()/user.mLastMediaButtonReceiver.send will aquire ActivityManagerService lock.
    
    Change-Id: Ia5dc1a867705b99748fee36ba0dbd50e52000081
    Signed-off-by: zhangshuxiao <zhangshuxiao@xiaomi.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/media/MediaSessionService.java
+++ services/core/java/com/android/server/media/MediaSessionService.java
@@ -1138,7 +1138,13 @@ public class MediaSessionService extends SystemService implements Monitor {
             @Override
             public void onSendFinished(PendingIntent pendingIntent, Intent intent, int resultCode,
                     String resultData, Bundle resultExtras) {
-                onReceiveResult(resultCode, null);
+                final int rc = resultCode;
+                mHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        onReceiveResult(rc, null);
+                    }
+                });
             }
         };
 
